6773e6649b7fb46dacf729e694d2792a35329692
Shubham K <subham.sinha41@gmail.com>
2021-03-10 00:01:40 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const gap = 10;
+const carousel = document.getElementById("carousel");
+console.log(carousel);
+const  content = document.getElementById("content");
+const  next = document.getElementById("next1");
+const  prev = document.getElementById("prev1");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
