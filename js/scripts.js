283c0e0d222b2bb999cfac10434f69ce598629c9
Shubham K <subham.sinha41@gmail.com>
2021-03-10 01:27:45 +0530

    new commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// CAROUSEL SCRIPTS
 const gap = 10;
 const carousel = document.getElementById("carousel");
 console.log(carousel);
@@ -26,3 +27,72 @@ prev.addEventListener("click", e => {
 
 let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+
+// MENU STICKY
+
+window.onscroll = function() {
+  myFunction()
+};
+
+var navbar = document.getElementsByClassName("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+// Slideshow Scripts 
+
+var slideIndex = 0;
+  showSlides();
+  var slides,dots;
+  
+  function showSlides() {
+      var i;
+      slides = document.getElementsByClassName("mySlides");
+      dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex> slides.length) {slideIndex = 1}    
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlides, 3000); // Change image every 8 seconds
+  }
+  
+  function plusSlides(position) {
+      slideIndex +=position;
+      if (slideIndex> slides.length) {slideIndex = 1}
+      else if(slideIndex<1){slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+  }
+  
+  function currentSlide(index) {
+      if (index> slides.length) {index = 1}
+      else if(index<1){index = slides.length}
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[index-1].style.display = "block";  
+      dots[index-1].className += " active";
+  }
